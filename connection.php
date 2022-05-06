5de6ce7ed05aefaf04f4fb57f32c28218174177d
Bahashtsalih <86877971+Bahashtsalih@users.noreply.github.com>
2022-05-06 15:13:59 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,12 +1,8 @@
 <?php      
-    // $host = "localhost";  
-    // $user = "root";  
-    // $password = '';  
-    // $db_name = "feed";  
-    $host = "sql102.epizy.com";  
-    $user = "epiz_31665495";  
-    $password = 'yBxyLAz8KI3';  
-    $db_name = "epiz_31665495_feed";  
+    $host = "localhost";  
+    $user = "root";  
+    $password = '';  
+    $db_name = "feed";  
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
         die("Failed to connect with MySQL: ". mysqli_connect_error());  
